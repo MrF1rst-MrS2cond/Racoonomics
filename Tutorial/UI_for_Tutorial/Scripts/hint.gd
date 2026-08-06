eaad925efa65c96607147bfc4554960eab40fd43
Nickel! <jjsmith239076@gmail.com>
2026-08-06 23:51:24 +0300

    HintBlankReady

--- Tutorial/UI_for_Tutorial/Scripts/hint.gd
+++ Tutorial/UI_for_Tutorial/Scripts/hint.gd
@@ -1,6 +1,8 @@
 extends Control
+class_name HintPanel
 
 @onready var video_player: VideoStreamPlayer = $TextureRect/VideoStreamPlayer
+@onready var text_label: RichTextLabel = $TextureRect/TextEdit
 @onready var close_button: Button = $TextureRect/BtClose
 
 @export var on_screen_x: float = 20.0
@@ -10,34 +12,37 @@ var off_screen_x: float = -500.0
 
 func _ready() -> void:
 	off_screen_x = -size.x - 50.0
-	
 	if close_button:
 		close_button.pressed.connect(hide_hint)
-	
+
+func setup(text: String, video_path: String = "") -> void:
+	if text_label:
+		text_label.text = text
+		
+	if video_player and video_path != "":
+		var stream_res = load(video_path) as VideoStream
+		if stream_res:
+			video_player.stream = stream_res
+			
 	show_hint()
 
 func show_hint() -> void:
 	position.x = off_screen_x
 	show()
-	
-	video_player.stop()
+	if video_player and video_player.stream:
+		video_player.play()
 	
 	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "position:x", on_screen_x, slide_duration)
-	
-	await tween.finished
-	
-	video_player.play()
 
 func hide_hint() -> void:
-	video_player.stop()
+	if video_player:
+		video_player.stop()
 	
 	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
 	tween.tween_property(self, "position:x", off_screen_x, slide_duration)
 	
 	await tween.finished
-	hide()
+	queue_free()
---
+++ Tutorial/UI_for_Tutorial/Scripts/tutorial_manager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var hint_scene: PackedScene = preload("res://Tutorial/UI_for_Tutorial/Scenes/Hint.tscn") 
+var current_hint: HintPanel = null
+
+func show_tutorial_hint(text: String, video_path: String = "") -> void:
+	if is_instance_valid(current_hint):
+		current_hint.hide_hint()
+		
+	var main_ui = get_tree().current_scene.find_child("Main_UI", true, false)
+	if main_ui:
+		current_hint = hint_scene.instantiate() as HintPanel
+		main_ui.add_child(current_hint)
+		current_hint.setup(text, video_path)
+
+func close_current_hint() -> void:
+	if is_instance_valid(current_hint):
+		current_hint.hide_hint()
+		current_hint = null
