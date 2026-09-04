a738925d5511a2586489abac502ee3a69a7327db
Baizard-13 <1_VSelenniy_13@mail.ru>
2026-09-04 17:34:16 +0300

    clikforfarm1and2levels

--- buildings/scripts/meat_farm.gd
+++ buildings/scripts/meat_farm.gd
@@ -1,5 +1,9 @@
 @tool
 extends FarmsClass
+
+var label_scene: PackedScene = preload("res://UI/scenes/clikfarm.tscn")
+var current_label: Node3D = null
+
 var world_grid : WorldGrid
 var animation_speed: float = 1.0
 
@@ -13,12 +17,38 @@ func _extends_ready() -> void:
 
 	if !is_ghost:
 		pass
-	#animation_player.play(&"Anim_Farm_Apple_lvl1|Anim_Farm_Apple_lvl1|AppleIdle",-1,animation_speed)
+
 
 func on_click_harvest():
 	click_queue += 1
+
+	_ensure_click_label()
+
+	_update_label_count()
+
 	if !animation_player.is_playing() or animation_player.current_animation != &"Anim_Farm_Meat_lvl1|Action_001":
 		_process_queue()
+
+
+func _ensure_click_label() -> void:
+	if is_ghost or Engine.is_editor_hint():
+		return
+
+	if is_instance_valid(current_label):
+		return
+
+	current_label = label_scene.instantiate() as Node3D
+	current_label.building_owner = self
+
+	get_tree().current_scene.add_child(current_label)
+
+	if current_label.has_method("update_label_position"):
+		current_label.update_label_position()
+
+
+func _update_label_count() -> void:
+	if is_instance_valid(current_label) and current_label.has_method("set_count"):
+		current_label.set_count(click_queue)
 
 
 func _process_queue() -> void:
@@ -27,3 +57,14 @@ func _process_queue() -> void:
 		await animation_player.animation_finished
 		storage[&"meat_out"].put(Global.get_type("meat"), 9)
 		click_queue -= 1
+
+		_update_label_count()
+
+	if is_instance_valid(current_label):
+		current_label.queue_free()
+		current_label = null
+
+
+func _exit_tree() -> void:
+	if is_instance_valid(current_label):
+		current_label.queue_free()
--- buildings/scripts/wood_farm.gd
+++ buildings/scripts/wood_farm.gd
@@ -1,5 +1,9 @@
 @tool
 extends FarmsClass
+
+var label_scene: PackedScene = preload("res://UI/scenes/clikfarm.tscn")
+var current_label: Node3D = null
+
 var animation_speed: float = 1.0
 
 var world_grid : WorldGrid
@@ -13,13 +17,39 @@ func _extends_ready() -> void:
 
 	if !is_ghost:
 		pass
-		#animation_player.play(&"Anim_Farm_Tree_lvl1|Anim_Tree_work_lvl1", -1, animation_speed)
+
 	animation_player.play(&"Anim_Farm_Tree_lvl1|Idle", -1, animation_speed)
 
 func on_click_harvest():
 	click_queue += 1
+
+	_ensure_click_label()
+
+	_update_label_count()
+
 	if !animation_player.is_playing() or animation_player.current_animation != &"Anim_Farm_Tree_lvl1|Anim_Farm_Tree_lvl1|Anim_Tree_work_lvl1":
 		_process_queue()
+
+
+func _ensure_click_label() -> void:
+	if is_ghost or Engine.is_editor_hint():
+		return
+
+	if is_instance_valid(current_label):
+		return
+
+	current_label = label_scene.instantiate() as Node3D
+	current_label.building_owner = self
+
+	get_tree().current_scene.add_child(current_label)
+
+	if current_label.has_method("update_label_position"):
+		current_label.update_label_position()
+
+
+func _update_label_count() -> void:
+	if is_instance_valid(current_label) and current_label.has_method("set_count"):
+		current_label.set_count(click_queue)
 
 
 func _process_queue() -> void:
@@ -29,7 +59,13 @@ func _process_queue() -> void:
 		storage[&"wood_out"].put(Global.get_type("wood"), 9)
 		click_queue -= 1
 
-#func tick_produce(tick: int) -> void:
-#	if tick % 3 == 0:
-#		storage[&"wood_out"].put(Global.get_type("wood"), 5)
-#		#print("запас дерева ", storage[&"wood_out"].stacks)
+		_update_label_count()
+
+	if is_instance_valid(current_label):
+		current_label.queue_free()
+		current_label = null
+
+
+func _exit_tree() -> void:
+	if is_instance_valid(current_label):
+		current_label.queue_free()
